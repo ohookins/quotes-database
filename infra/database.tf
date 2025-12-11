828e329ffdaa0fb6dc454a61e428a0ba27d16aae
Oliver Hookins <ohookins@gmail.com>
2025-12-11 19:55:42 +1100

    Fix database instances and security group.

--- infra/database.tf
+++ infra/database.tf
@@ -8,15 +8,15 @@ resource "random_password" "database_password" {
 }
 
 resource "aws_rds_cluster" "main" {
-  cluster_identifier   = "quotes"
-  engine               = "aurora-postgresql"
-  engine_mode          = "provisioned"
-  engine_version       = "17.4"
-  database_name        = "quotes"
-  db_subnet_group_name = aws_db_subnet_group.main.name
-  master_username      = random_pet.database_username.id
-  master_password      = random_password.database_password.result
-
+  cluster_identifier     = "quotes"
+  engine                 = "aurora-postgresql"
+  engine_mode            = "provisioned"
+  engine_version         = "17.4"
+  database_name          = "quotes"
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  master_username        = random_pet.database_username.id
+  master_password        = random_password.database_password.result
+  vpc_security_group_ids = [aws_vpc_security_group.database.id]
 
   serverlessv2_scaling_configuration {
     max_capacity             = 1.0
@@ -26,7 +26,10 @@ resource "aws_rds_cluster" "main" {
 }
 
 resource "aws_rds_cluster_instance" "main" {
+  count = 2
+
   cluster_identifier = aws_rds_cluster.main.id
+  identifier_prefix  = "quotes-instance-"
   instance_class     = "db.serverless"
   engine             = aws_rds_cluster.main.engine
   engine_version     = aws_rds_cluster.main.engine_version
@@ -39,4 +42,22 @@ resource "aws_db_subnet_group" "main" {
   tags = {
     Name = "Private DB subnet group"
   }
+}
+
+resource "aws_vpc_security_group" "database" {
+  name        = "quotes-database-sg"
+  description = "Security group for Aurora PostgreSQL cluster"
+  vpc_id      = aws_vpc.main.id
+  tags = {
+    Name = "quotes-database-sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "database_ingress" {
+  security_group_id = aws_vpc_security_group.database.id
+  description       = "PostgreSQL from VPC"
+  from_port         = 5432
+  to_port           = 5432
+  ip_protocol       = "tcp"
+  cidr_ipv4         = local.private_block
 }
