23efb5b67389982be2b7c68e43aad5fbd61c805e
Oliver Hookins <ohookins@gmail.com>
2025-12-11 18:35:42 +1100

    Make the CIDRs easier to understand (and actually work).

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,6 +1,9 @@
 locals {
-  subnet_cidrs = cidrsubnets(var.vpc_cidr, 2, 2)
+  subnet_cidrs  = cidrsubnets(var.vpc_cidr, 1, 1)
+  public_block  = local.subnet_cidrs[0]
+  private_block = local.subnet_cidrs[1]
+
   # subnet_cidrs[0] and [1]: 2 blocks, then split each into 2 for 4 subnets
-  public_subnet_cidrs  = [cidrsubnets(local.subnet_cidrs[0], 1)[0], cidrsubnets(local.subnet_cidrs[0], 1)[1]]
-  private_subnet_cidrs = [cidrsubnets(local.subnet_cidrs[1], 1)[0], cidrsubnets(local.subnet_cidrs[1], 1)[1]]
+  public_subnet_cidrs  = cidrsubnets(local.public_block, 1, 1)
+  private_subnet_cidrs = cidrsubnets(local.private_block, 1, 1)
 }
