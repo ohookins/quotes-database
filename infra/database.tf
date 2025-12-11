8f6fbb31b0d22f95845024afbd4190284111167e
Oliver Hookins <ohookins@gmail.com>
2025-12-11 19:57:52 +1100

    Correct resource name.

--- infra/database.tf
+++ infra/database.tf
@@ -16,7 +16,7 @@ resource "aws_rds_cluster" "main" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
   master_username        = random_pet.database_username.id
   master_password        = random_password.database_password.result
-  vpc_security_group_ids = [aws_vpc_security_group.database.id]
+  vpc_security_group_ids = [aws_security_group.database.id]
 
   serverlessv2_scaling_configuration {
     max_capacity             = 1.0
@@ -44,7 +44,7 @@ resource "aws_db_subnet_group" "main" {
   }
 }
 
-resource "aws_vpc_security_group" "database" {
+resource "aws_security_group" "database" {
   name        = "quotes-database-sg"
   description = "Security group for Aurora PostgreSQL cluster"
   vpc_id      = aws_vpc.main.id
@@ -54,7 +54,7 @@ resource "aws_vpc_security_group" "database" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "database_ingress" {
-  security_group_id = aws_vpc_security_group.database.id
+  security_group_id = aws_security_group.database.id
   description       = "PostgreSQL from VPC"
   from_port         = 5432
   to_port           = 5432
