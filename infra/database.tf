84643e2799568f25412207b36c1de0a2c55f4b3a
Oliver Hookins <ohookins@users.noreply.github.com>
2025-12-11 00:43:12 -0800

    Add database subnets. (#6)
    * Add database subnets.
    
    * Rename pipeline.

--- infra/database.tf
+++ infra/database.tf
@@ -5,13 +5,14 @@ resource "random_password" "database_password" {
 }
 
 resource "aws_rds_cluster" "main" {
-  cluster_identifier = "quotes"
-  engine             = "aurora-postgresql"
-  engine_mode        = "provisioned"
-  engine_version     = "17.4"
-  database_name      = "quotes"
-  master_username    = random_pet.database_username.id
-  master_password    = random_password.database_password.result
+  cluster_identifier   = "quotes"
+  engine               = "aurora-postgresql"
+  engine_mode          = "provisioned"
+  engine_version       = "17.4"
+  database_name        = "quotes"
+  db_subnet_group_name = aws_db_subnet_group.main.name
+  master_username      = random_pet.database_username.id
+  master_password      = random_password.database_password.result
 
 
   serverlessv2_scaling_configuration {
@@ -26,4 +27,13 @@ resource "aws_rds_cluster_instance" "main" {
   instance_class     = "db.serverless"
   engine             = aws_rds_cluster.main.engine
   engine_version     = aws_rds_cluster.main.engine_version
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "main"
+  subnet_ids = aws_subnet.private.*.id
+
+  tags = {
+    Name = "Private DB subnet group"
+  }
 }
