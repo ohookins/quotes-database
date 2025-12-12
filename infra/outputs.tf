9eeb7562c3cce21411f1d173ddb36d7424d1c28d
Oliver Hookins <ohookins@gmail.com>
2025-12-13 09:06:20 +1100

    Add the service URL to outputs.

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,8 @@
 output "ecr_url" {
   value = aws_ecr_repository.main.repository_url
 }
+
+output "service_url" {
+  description = "Publicly accessible URL of the AppRunner service"
+  value       = aws_apprunner_service.main.service_url
+}
